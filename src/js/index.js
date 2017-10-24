f95a87b1c20c2997e026d9fa784fc573c370bffd
yaominguo <missgmy@yahoo.com>
2017-10-25 00:07:12 +0800

    算法

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 /**
  * Created by MrGuo on 2017/10/24.
  */
+
+const toolKit = require('./toolkit');
+const matrix = toolKit.makeMatrix();
+console.log(matrix);
+
+const a=Array.from({length:9},(v,i)=>i);
+console.log(a);
+console.log(toolKit.shuffle(a));
---
+++ src/js/toolkit.js
@@ -0,0 +1,27 @@
+/**
+ * Created by MrGuo on 2017/10/24.
+ */
+
+const matrixToolKit = {
+    makeRow(v = 0){
+        const array = new Array(9);
+        array.fill(v);
+        return array;
+    },
+
+    makeMatrix(v = 0){
+        return Array.from({length: 9}, () => this.makeRow(v));
+    },
+
+    /*Fisher-Yates 洗牌法*/
+    shuffle(array){
+        const endIndex = array.length - 2;
+        for (let i = 0; i < endIndex; i++) {
+            const j = i + Math.floor(Math.random() * (array.length - i));
+            [array[i], array[j]] = [array[j], array[j]];
+        }
+        return array;
+    },
+};
+
+module.exports = matrixToolKit;
